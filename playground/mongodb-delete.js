82281bb179950a23b5cb57e0db2c5d580e6df29c
wei <wei@weis-MacBook-Air.local>
2018-04-09 21:17:13 -0500

    add delete and update file

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,49 @@
+// const MongoClient = require('mongodb').MongoClient;
+// const ObjectID= require('mongodb').ObjectID;
+const {MongoClient, ObjectID} = require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB Server.', err);
+    }
+
+    console.log('Successfully connected to MongoDB Server.');
+
+    const db = client.db('TodoApp');
+    // db.collection('Todos').find({completed:true}).toArray().then((docs) => {
+    
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+  
+    // }, (err) => {
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+    
+//     db.collection('Todos').find().count().then((count) => {
+//         console.log(`Todos count: ${count}`);
+  
+//     }, (err) => {
+
+//         console.log(JSON.stringify(result.ops, undefined, 2));
+//     });
+
+//     db.collection('Todos').find({
+//         _id: new ObjectID('5aca9745f706eab2a00c57b7')
+//     }).toArray().then((docs) => {
+    
+//         console.log('Todos');
+//         console.log(JSON.stringify(docs, undefined, 2));
+  
+//     }, (err) => {
+// ''
+//         console.log(JSON.stringify(result.ops, undefined, 2));
+//     });
+
+
+    db.collection('Todos').findOneAndDelete({text: 'Something to do'}).then((result) => {
+        console.log(result);
+    });
+    
+
+    //client.close();
+});
