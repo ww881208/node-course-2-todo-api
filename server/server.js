17b508f8877af3bebf9f9f40232cf1c1d660f01e
wei <wei@weis-MacBook-Air.local>
2018-04-25 20:09:42 -0500

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.port || 3000;
 
 app.use(bodyParser.json());
 
@@ -54,8 +55,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server start at the port 3000.');
+app.listen(port, () => {
+    console.log('Server start at the port: ' + port);
 })
 
 module.exports = {app};
