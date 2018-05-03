3c83f10f58a295b112168dea14e6d528e9422d5e
wei <wei@weis-MacBook-Air.local>
2018-05-02 22:33:16 -0500

    Add GET /users/me route and authencate middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -131,6 +132,11 @@ app.post('/users', (req, res) => {
     })
 });
 
+
+app.get('/users/me', authenticate, (req, res) => {
+
+    res.send(req.user);
+});
 
 app.listen(port, () => {
     console.log('Server start at the port: ' + port);
