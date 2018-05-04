bb69c16e5ec2b0ee01bf9164cfd46c0e40392261
wei <wei@weis-MacBook-Air.local>
2018-05-03 20:53:29 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,11 +1,13 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var message = 'this is the plain text';
 var hash = SHA256(message).toString();
 
 console.log('Message: ', message);
 console.log('Hash: ', hash);
+
 
 var data = {
     id: 4
@@ -32,3 +34,17 @@ console.log('jwttoken: ', jwttoken);
 
 var decode = jwt.verify(jwttoken, 'secrete');
 console.log('decoded: ', decode);
+
+var pwd = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(pwd, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPwd = '$2a$10$FdE2Wf1ZzdOicUGDO1mylu7ZFIDRBkRfenfWVqu/aTXwYaP1nc6SG'
+
+bcrypt.compare(pwd, hashedPwd, (err, res) => {
+    console.log(res);
+});
